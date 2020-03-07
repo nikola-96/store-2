a6992015c69d1811687390558ddf725696171d34
Nikola <nikolaviki@hotmail.com>
2020-03-07 16:26:58 +0100

    Zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <b-nav tabs align="center">
+        <b-nav-item active><router-link to="/">Products</router-link></b-nav-item>
+        <b-nav-item><router-link to="/customers">Customers</router-link></b-nav-item>
+      </b-nav>
     </div>
     <router-view/>
   </div>
@@ -17,16 +19,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
